b661c7348194ced9888d0b907c14498461d38eee
domminatrix <53867203+domminatrix@users.noreply.github.com>
2019-11-27 08:01:01 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ kubectl delete namespace domminatrix
 kubectl create namespace domminatrix
 
 kubectl apply -n domminatrix -f redis-sts.yaml
-
 kubectl apply -n domminatrix -f redis-svc.yaml
 
+kubectl describe pods -n domminatrix redis-cluster-0
